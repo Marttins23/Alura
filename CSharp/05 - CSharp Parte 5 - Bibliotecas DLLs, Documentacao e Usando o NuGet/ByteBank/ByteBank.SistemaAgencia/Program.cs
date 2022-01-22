96ff93b6fb3549cfb6be578f0df647ee7a2dba58
Marttins23 <mateus_martins18@yahoo.com.br>
2022-01-21 23:15:20 -0300

    Primeira Parte do Curso 5

---
+++ CSharp/05 - CSharp Parte 5 - Bibliotecas DLLs, Documentacao e Usando o NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,27 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente(865, 865748);
+
+            Console.WriteLine(conta.Numero);
+
+            FuncionarioAutenticavel mateus = new GerenteDeConta("104.488.306-55"); 
+
+            Console.WriteLine("\nPressione enter para sair...");
+            Console.ReadLine();
+            
+        }
+    }
+}
