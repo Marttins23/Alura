333f5fe38b03f66ba2ce22ec787ef635c251a4a9
Marttins23 <mateus_martins18@yahoo.com.br>
2022-01-24 23:51:04 -0300

    Primeira Parte do Curso 7 Adicionada

---
+++ CSharp/06 - CSharp Parte 7 - Arrays e Tipos Genéricos/ByteBank.SistemaAgencia/ExpressaoRegular.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public static class ExpressaoRegular
+    {
+        public static string RetornaPadrao(string padrao, string texto)
+        {
+            //Para capturar um número de telefone no formato XXXX-XXXX
+            //ou XXXXX-XXXX, podemos utilizar os seguintes padrão de
+            //Expressões Regulares:
+            // "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"
+            // "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]"
+            // "[0-9]{4,5}[-][0-9]{4}"
+            // "[0-9]{4,5}[-]{0,1}[0-9]{4}"
+            // "[0-9]{4,5}-{0,1}[0-9]{4}"
+            // "[0-9]{4,5}-?[0-9]{4}"
+
+            Match resultado = Regex.Match(texto, padrao);
+
+            return resultado.Value;
+
+        }
+
+    }
+}
---
+++ CSharp/06 - CSharp Parte 7 - Arrays e Tipos Genéricos/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDeContaCorrente lista = new ListaDeContaCorrente(3);
+
+            ContaCorrente contaJoao = new ContaCorrente("Joao", 876, 756473);
+
+            lista.Adicionar(contaJoao);
+            lista.Adicionar(new ContaCorrente("Mateus", 865, 865748));
+            lista.Adicionar(new ContaCorrente("Mateus", 865, 865748));
+            lista.Adicionar(new ContaCorrente("Mateus", 865, 865748));
+            lista.Adicionar(new ContaCorrente("Mateus", 865, 865748));
+            lista.Adicionar(new ContaCorrente("Mateus", 865, 865748));
+            //lista.ImprimeContas();
+
+            for(int i = 0; i < lista.Tamanho; i++)
+            {
+                try
+                {
+                    ContaCorrente contaAtual = lista.GetConta(i);
+                    Console.WriteLine(contaAtual.ToString());
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                
+            }
+
+            //lista.Remover(contaJoao);
+            //lista.ImprimeContas();
+
+            Console.WriteLine("\nPressione enter para sair...");
+            Console.ReadLine();
+        }
+
+        public static void TestaContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente("Mateus", 865, 876543),
+                new ContaCorrente("Fernanda", 865, 875674),
+                new ContaCorrente("Felipe", 865, 847328)
+            };
+
+            for (int i = 0; i < contas.Length; i++)
+            {
+                Console.WriteLine($"A conta no índice {i} possui o número igual a {contas[i].Numero}");
+            }
+        }
+
+        public static void TestaArrayInt()
+        {
+            int[] idades = new int[5];
+
+            idades[0] = 23;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            int soma = 0;
+
+            for (int i = 0; i < idades.Length; i++)
+            {
+                soma += idades[i];
+            }
+
+            int media = soma / idades.Length;
+
+            Console.WriteLine($"A média das idades é {media} anos.");
+        }
+    }
+}
+                                                            
