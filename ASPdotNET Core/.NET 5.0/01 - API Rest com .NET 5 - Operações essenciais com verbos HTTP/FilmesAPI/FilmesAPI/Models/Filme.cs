50b436b90daaa67b3844a556138766f090476dd9
Marttins23 <mateus_martins18@yahoo.com.br>
2022-02-10 20:59:16 -0300

    Primeira Parte Adicionada.

---
+++ ASPdotNET Core/.NET 5.0/01 - API Rest com .NET 5 - Operações essenciais com verbos HTTP/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; internal set; }
+        [Required(ErrorMessage = "O campo Título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O Gênero não pode ultrapassar 30 caracteres.")]
+        public string Genero { get; set; }
+        [Range(1 ,600, ErrorMessage = "A Duração deve ter no mínimo 1 e no máximo 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
