aeef49c79d895268566161ea5bae660c0af49c99
Marttins23 <mateus_martins18@yahoo.com.br>
2022-02-19 20:03:37 -0300

    Curso 7 Adicionado

--- PHP/07 - PHP Composer - Dependências, Autoload e Publicação/buscar-cursos.php
+++ PHP/07 - PHP Composer - Dependências, Autoload e Publicação/buscar-cursos.php
@@ -1,10 +1,24 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
-$client =  new Client();
+$client =  new Client(
+    [
+        'verify' => false,
+        'base_uri' => 'https://www.alura.com.br/'
+    ]
+);
 
-$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+$crawler = new Crawler();
 
-echo $resposta->getBody();
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach($cursos as $curso) {
+    exibeMensagem($curso);
+}
 
