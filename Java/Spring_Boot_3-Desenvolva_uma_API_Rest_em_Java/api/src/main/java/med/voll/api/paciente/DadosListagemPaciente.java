f4297521825f43446d6bf9fc5e7bf5686634d650
Marttins23 <mateus_martins18@yahoo.com.br>
2023-12-26 18:38:29 -0300

    Curso 1 de APIs com Spring Boot finalizado.

---
+++ Java/Spring_Boot_3-Desenvolva_uma_API_Rest_em_Java/api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,26 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.Embedded;
+import med.voll.api.endereco.Endereco;
+
+public record DadosListagemPaciente(
+        Long id,
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        Endereco endereco
+) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(
+                paciente.getId(),
+                paciente.getNome(),
+                paciente.getEmail(),
+                paciente.getTelefone(),
+                paciente.getCpf(),
+                paciente.getEndereco()
+        );
+    }
+
+}
